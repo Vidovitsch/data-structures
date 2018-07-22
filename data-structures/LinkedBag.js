9884d6ecdf2d10056096241bdce2f4f2b9aeccde
Vidovitsch <david.de.bekker@gmail.com>
2018-07-22 22:57:27 +0200

    LinkedBag.js added and implemented

---
+++ data-structures/LinkedBag.js
@@ -0,0 +1,51 @@
+let head = null;
+
+function LinkedBag() {
+  this.length = 0;
+}
+
+/**
+ * Node that fills an index in the bag.
+ *
+ * @param       {Any} value the value (item) to be added to this bag
+ * @constructor
+ */
+function Node(value) {
+  this.value = value;
+  this.next = null;
+}
+
+const L = LinkedBag.prototype;
+
+/**
+ * Adds a value to this bag.
+ *
+ * Time complexity (worst): O(1)
+ *
+ * @param {Any} value the value to be added to this bag
+ */
+L.add = function add(value) {
+  const oldHead = head;
+  head = new Node(value);
+  head.next = oldHead;
+  this.length += 1;
+};
+
+/**
+ * Returns an array of the items in this bag.
+ *
+ * Time complexity: O(n)
+ *
+ * @return {Any[]} Array of items in this bag
+ */
+L.asArray = function asArray() {
+  const arr = new Array(this.length);
+  let node = head;
+  while (node.next) {
+    arr.push(node.value);
+    node = node.next;
+  }
+  return arr;
+};
+
+module.exports = LinkedBag;
